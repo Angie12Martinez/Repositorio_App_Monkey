31e7c9d663ae6ea14539170f305c5dadc6187ff1
unknown <angelicamartinezmiranda5@gmail.com?>
2022-04-10 13:05:23 -0500

    Subiendo mi pantalla Monkey

---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_application_1/src/pages/login_page.dart';
+import 'package:flutter_application_1/src/pages/home_page.dart';
+import 'package:flutter/cupertino.dart';
+
+
+final routes = <String, WidgetBuilder>{
+  '/': (BuildContext context) => const HomePage(),
+  '/login': (BuildContext context) => const LoginPage(),
+};
